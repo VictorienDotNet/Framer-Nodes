16b5ac6283c295f4fb303b029d49b429518f44ca
Victorien <bvictorien@pm.me>
2024-05-27 16:13:27 +0200

    Update Sum.tsx

--- examples/Sum.tsx
+++ examples/Sum.tsx
@@ -1,13 +1,13 @@
 import { ControlType, addPropertyControls } from "framer"
 import { NodeTemplate } from "https://framer.com/m/UseNode-U5fM.js"
 
+const transform = (values) => {
+    return values.reduce(function (sum, value) {
+        return sum + value
+    }, 0)
+}
+
 export default function Sum(props) {
-    const transform = (values) => {
-        const sum = values.reduce(function (sum, value) {
-            return sum + value
-        }, 0)
-        return sum
-    }
     return <NodeTemplate {...props} label="Sum" transform={transform} />
 }
 
