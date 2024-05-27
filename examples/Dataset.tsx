bf70c5d6bce1021baeac9e22f8f64ec99509f83c
Victorien <bvictorien@pm.me>
2024-05-27 15:58:27 +0200

    Update Dataset.tsx

--- examples/Dataset.tsx
+++ examples/Dataset.tsx
@@ -1,1 +1,36 @@
-//Code here
+import { ControlType, addPropertyControls } from "framer"
+import { useEffect, useState, useContext } from "react"
+import {
+    NodeDesignTemplate,
+    useNodeOutput,
+} from "https://framer.com/m/UseNode-U5fM.js"
+
+export default function Dataset({ label, values }) {
+    const [output, setOutput, deleteOutput] = useNodeOutput(
+        values && values.split(",")
+    )
+
+    useEffect(() => {
+        return () => {
+            deleteOutput()
+        }
+    }, [])
+
+    //return value
+    return (
+        <NodeDesignTemplate variant="Value" icon="Collection" label={label} />
+    )
+}
+
+addPropertyControls(Dataset, {
+    label: {
+        type: ControlType.String,
+        defaultValue: "Dataset",
+    },
+    values: {
+        title: "Values",
+        type: ControlType.String,
+        displayTextArea: true,
+        description: "Values separated by a ,",
+    },
+})
